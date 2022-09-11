e9beca4a7ba36483e968dff6411d73f92d1cda75
KhyPhay <khy.phat@student.passerellesnumeriques.org>
2022-09-11 17:44:39 +0700

    #12: fix bug of dublicate classes

--- front/src/components/alumni/profile/my_profile/MyProfileContainer.vue
+++ front/src/components/alumni/profile/my_profile/MyProfileContainer.vue
@@ -1,9 +1,7 @@
 <template>
     <div class=" mt-[20px]">
         <profile-alumni @click-popup="isShow = !isShow"> </profile-alumni>
-    </div>
-    <div v-if="isShow" >
-        <my-profile-popup @click-popup="isShow = !isShow"></my-profile-popup>
+        <my-profile-popup v-if="isShow" @click-popup="isShow = !isShow"></my-profile-popup>
     </div>
 </template>
 
--- front/src/components/alumni/profile/work_experience/WorkExperiencePopup.vue
+++ front/src/components/alumni/profile/work_experience/WorkExperiencePopup.vue
@@ -7,12 +7,12 @@
                     <div class="w-full bg-white font-medium">
                         <span class="bg-white">Companies:</span>
                         <div class="flex bg-white mt-1">
-                            <span class="bg-white inline-flex items-center p-2 text-sm  bg-white rounded-l-md border-2 border-r-0 border-gray-300">
+                            <span class="bg-white inline-flex items-center p-2 rounded-l-md border-2 border-r-0 border-gray-300">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[26px] h-[26px] text-white bg-sky rounded-full p-1">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
                                 </svg>
                             </span>
-                            <select id="" class="rounded-none bg-white cursor-pointer font-normal text-base  block flex-1 min-w-0 w-full text-sm border-gray-300 border-2 p-2 rounded-r-md">
+                            <select id="" class="rounded-none bg-white cursor-pointer font-normal text-base  block flex-1 min-w-0 w-full border-gray-300 border-2 p-2 rounded-r-md">
                                 <option class="cursor-pointer" v-for="company of companies" :key="company" :value="company">{{company}}</option>
                             </select>
                         </div>
@@ -22,10 +22,10 @@
                     <div class="w-full bg-white font-medium">
                         <span class="bg-white">Position:</span>
                         <div class="flex bg-white mt-1">
-                            <span class="bg-white inline-flex items-center p-2 text-sm  bg-white rounded-l-md border-2 border-r-0 border-gray-300">
+                            <span class="bg-white inline-flex items-center p-2 rounded-l-md border-2 border-r-0 border-gray-300">
                                 <img class="w-[26px] h-[26px]"  src="../../../../assets/position.png" alt="">
                             </span>
-                            <input type="text" id="website-admin" placeholder="position" class="rounded-none pl-3 bg-white font-normal text-base  block flex-1 min-w-0 w-full text-sm border-gray-300 border-2 p-2 rounded-r-md" >
+                            <input type="text" id="website-admin" placeholder="position" class="rounded-none pl-3 bg-white font-normal text-base  block flex-1 min-w-0 w-full border-gray-300 border-2 p-2 rounded-r-md" >
                         </div>
                     </div>
                 </div>
@@ -33,23 +33,23 @@
                     <div class="w-2/4 bg-white font-medium mr-2">
                         <span class="bg-white">Start date:</span>
                         <div class="flex bg-white mt-1">
-                            <span class="bg-white inline-flex items-center p-2 text-sm  bg-white rounded-l-md border-2 border-r-0 border-gray-300">
+                            <span class="bg-white inline-flex items-center p-2 text-sm rounded-l-md border-2 border-r-0 border-gray-300">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[26px] h-[26px] text-white bg-sky rounded-full p-1">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
                                 </svg>
                             </span>
-                            <input type="date" id="website-admin" class="rounded-none bg-white font-normal text-base  block flex-1 min-w-0 w-full text-sm border-gray-300 border-2 p-2 rounded-r-md" placeholder="email">
+                            <input type="date" id="website-admin" class="rounded-none bg-white font-normal text-base  block flex-1 min-w-0 w-full border-gray-300 border-2 p-2 rounded-r-md" placeholder="email">
                         </div>
                     </div>
                     <div class="w-2/4 bg-white font-medium ml-2">
                         <span class="bg-white">End date:</span>
                         <div class="flex bg-white mt-1">
-                            <span class="bg-white inline-flex items-center p-2 text-sm  bg-white rounded-l-md border-2 border-r-0 border-gray-300">
+                            <span class="inline-flex items-center p-2 text-sm bg-white rounded-l-md border-2 border-r-0 border-gray-300">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[26px] h-[26px] text-white bg-sky rounded-full p-1">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
                                 </svg>
                             </span>
-                            <input type="date" id="website-admin" class="rounded-none bg-white font-normal text-base  block flex-1 min-w-0 w-full text-sm border-gray-300 border-2 p-2 rounded-r-md" >
+                            <input type="date" id="website-admin" class="rounded-none bg-white font-normal text-base block flex-1 min-w-0 w-full border-gray-300 border-2 p-2 rounded-r-md" >
                         </div>
                     </div>
                 </div>
