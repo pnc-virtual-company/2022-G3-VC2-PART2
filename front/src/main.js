d75209d0db4fcb5bf7403c0fdd7880b2d39fa9d0
thibtik <thibtik123@gmail.com>
2022-09-13 19:32:36 +0700

    #24 : Add validation to update general information

--- front/src/main.js
+++ front/src/main.js
@@ -11,6 +11,7 @@ import PopupComponent from '@/components/popup_components/PopupComponent';
 import ButtonComponent from '@/components/button_components/ButtonComponent'
 import ProfileImagesComponent from '@/components/profile_images/ProfileImagesComponent'
 import GeneralInformationComponent from '@/components/general_information/GeneralInformationComponent';
+import AlertMissing from '@/components/alert_component/AlertMissing.vue';
 // alumni 
 import GeneralInformationContainer from './views/alumni/general_information/GeneralInformationContainer'
 import GeneralInformation from './views/alumni/general_information/GeneralInformation';
@@ -18,12 +19,9 @@ import AlumniProfileContainer from './views/alumni/ProfileView.vue';
 import WorkExperience from './views/alumni/work_experience/WorkExperience'
 import WorkExperienceContainer from './views/alumni/work_experience/WorkExperienceContainer'
 import CardInformations from '@/components/card_components/CardInformations'
-import EducationBackground from './views/alumni/education_background/EducationBackground'
-import EducationBackgroundContainer from './views/alumni/education_background/EducationBackgroundContainer'
 // alumni popup components
 import GeneralInformationPopup from './views/alumni/general_information/GeneralInformationPopup'
 import WorkExperiencePopup from './views/alumni/work_experience/WorkExperiencePopup'
-import EducationBackgroundPopup from './views/alumni/education_background/EducationBackgroundPopup'
 
 // icon informations
 import TheEmail from '@/components/general_information/icon_informations/TheEmail';
@@ -43,6 +41,7 @@ app.component('card-components',CardComponent);
 app.component('button-components', ButtonComponent);
 app.component('profile-images-component',ProfileImagesComponent)
 app.component('general-information-component',GeneralInformationComponent)
+app.component('alert_missing',AlertMissing)
 // alumni Profile
 app.component('general-information',GeneralInformation)
 app.component('alumniProfile-container',AlumniProfileContainer)
@@ -50,13 +49,12 @@ app.component('work-experience', WorkExperience);
 app.component('work-experience-container', WorkExperienceContainer);
 app.component('card-informations', CardInformations)
 app.component("general-information-container", GeneralInformationContainer);
-app.component("education_background", EducationBackground);
-app.component("education_background_container", EducationBackgroundContainer);
+
 
 // alumni popup components register
 app.component('general-information-popup', GeneralInformationPopup);
 app.component('work-experience-popup', WorkExperiencePopup);
-app.component("education_background_popup", EducationBackgroundPopup);
+
 
 // icon informations
 app.component('the-email', TheEmail);
