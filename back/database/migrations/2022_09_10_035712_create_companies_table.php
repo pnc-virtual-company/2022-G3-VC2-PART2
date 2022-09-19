a0a4fb0cb089bf1008e607a9d49e156f67933a5a
Phearun-chhun <phearun.chhun@student.passerellesnumeriques.org>
2022-09-19 14:49:46 +0700

    #4 : update table

--- back/database/migrations/2022_09_10_035712_create_companies_table.php
+++ back/database/migrations/2022_09_10_035712_create_companies_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('location');
-            $table->string('logo');
+            $table->string('address');
+            $table->string('logo')->nullable();
             $table->string('link')->nullable();
             $table->timestamps();
         });
--- back/database/migrations/2022_09_10_035925_create_schools_table.php
+++ back/database/migrations/2022_09_10_035925_create_schools_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('schools', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('location');
-            $table->string('logo');
+            $table->string('addresse');
+            $table->string('logo')->nullable();
             $table->string('link')->nullable();
             $table->timestamps();
         });
