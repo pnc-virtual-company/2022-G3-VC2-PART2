8892105355a6b56a11f4bc4f2a11282583804d72
Phearun-chhun <phearun.chhun@student.passerellesnumeriques.org>
2022-09-19 15:50:18 +0700

    #4 : Update table

--- back/database/migrations/2022_09_10_035925_create_schools_table.php
+++ back/database/migrations/2022_09_10_035925_create_schools_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('schools', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('addresse');
+            $table->string('address');
             $table->string('logo')->nullable();
             $table->string('link')->nullable();
             $table->timestamps();
