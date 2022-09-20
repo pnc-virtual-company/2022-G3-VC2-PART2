243640b5b66f447b69c2ee51caaef650d26c7110
Phearun-chhun <phearun.chhun@student.passerellesnumeriques.org>
2022-09-20 18:49:11 +0700

    #54 : update heigh of card

--- front/src/views/admin/ERO_controll/ListEROView.vue
+++ front/src/views/admin/ERO_controll/ListEROView.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="w-10/12 m-auto">
-        <input-search></input-search>
+    <div class="w-[83%] m-auto ">
+            <input-search></input-search>
         <div class="mt-4 flex justify-end">
             <button-components @click="$emit('click-popup')" class="py-2 bg-sky text-[1rem] text-white hover:bg-white hover:text-sky-hover">Invite</button-components>
         </div>
         <div class="" >
-            <card-list-ERO v-for="ero of data.eroList" :key="ero" @delete-item="data.deleteEro(ero.id)">
+            <card-list-ERO v-for="ero of data.eroList" :key="ero" @delete-item="data.deleteEro(ero.id)" >
                 <template #title-1 >
-                    <div class="">
-                        <img alt="Vue logo" src="../../../assets/logo.png" width="50">
+                    <div class="flex justify-center aling-items-sm-center">
+                        <img alt="" src="../../../assets/logo.png" class=" w-[55px] "/>
                     </div>
                 </template>
                 <template #title-2>name</template>
