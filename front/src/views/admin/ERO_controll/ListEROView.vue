dd5e8cdf54b2b5f23834e9e561c125d18d5727e3
Phearun-chhun <phearun.chhun@student.passerellesnumeriques.org>
2022-09-20 16:09:12 +0700

    #53 : Update font size

--- front/src/views/admin/ERO_controll/ListEROView.vue
+++ front/src/views/admin/ERO_controll/ListEROView.vue
@@ -2,7 +2,7 @@
     <div class="w-10/12 m-auto">
         <input-search></input-search>
         <div class="mt-4 flex justify-end">
-            <button-components @click="$emit('click-popup')" class="py-2 bg-sky text-white hover:bg-sky-hover hover:text-white font-bold">Invite</button-components>
+            <button-components @click="$emit('click-popup')" class="py-2 bg-sky text-[1rem] text-white hover:bg-white hover:text-sky-hover">Invite</button-components>
         </div>
         <div class="" >
             <card-list-ERO v-for="i in 3" :key="i" class="">
@@ -10,8 +10,6 @@
                 <template #text-1 >thib tik</template>
                 <template #title-2> email</template>
                 <template #text-2>phearunchhun@gmail.com </template>
-                <template #title-4>gender </template>
-                <template #text-4>Male</template>
                 <template #title-5>Status </template>
                 <template #text-5>Registered</template>
             </card-list-ERO>
