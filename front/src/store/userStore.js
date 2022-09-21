17a72d6d073af460ad3443641cace1d609168065
sreyleakel <sreyleakel.official@gmail.com>
2022-09-21 10:25:14 +0700

    #70 : Search alumni & Ero in management view front

--- front/src/store/userStore.js
+++ front/src/store/userStore.js
@@ -26,7 +26,7 @@ export const userInformations = defineStore('get-data', {
   },
   actions: {
     getUserData(){
-      axios.get('/users/'+2).then((res)=>{
+      axios.get('/users/'+1).then((res)=>{
         this.userStore = res.data;
       })
     },
--- front/src/views/admin/ERO_controll/ListEROView.vue
+++ front/src/views/admin/ERO_controll/ListEROView.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="w-10/12 m-auto">
-        <input-search></input-search>
+        <input-search @search-keyword="updateKeyword" ></input-search>
         <div class="mt-4 flex justify-end">
             <button-components @click="$emit('click-popup')" class="py-2 bg-sky text-white hover:bg-sky-hover hover:text-white font-bold">Invite</button-components>
         </div>
         <div class="" >
-            <card-list-ERO v-for="ero of data.eroList" :key="ero" @delete-item="data.deleteEro(ero.id)">
+            <card-list-ERO v-for="ero of filteredEro" :key="ero" @delete-item="data.deleteEro(ero.id)">
                 <template #title-1 >
                     <div class="">
                         <img alt="Vue logo" src="../../../assets/logo.png" width="50">
@@ -21,7 +21,8 @@
 </template>
 
 <script>
-    import {userInformations} from "../../../store/userStore"
+    
+    import {userInformations} from "@/store/userStore"
     export default {
         setup(){
             const data = userInformations();
@@ -29,6 +30,48 @@
                 data,
             }
         },
-    
+
+        data() {
+            return {
+                keyword: null,
+            }
+        },
+
+        methods: {
+            updateKeyword(newKeyword) {
+                this.keyword = newKeyword;
+            }
+        },
+
+        computed: {
+            filteredEro() {
+                let eroList = [];
+                if (this.keyword) {
+                    let splitedKeywords = this.keyword.split(' ');
+                    this.data.eroList.forEach(eachEro => {
+                        let isIncludesKeyword = false;
+                        splitedKeywords.forEach(eachKeyword => {
+                            if (eachKeyword) {
+                                if (
+                                    (eachEro.first_name.toLowerCase()).includes(eachKeyword.toLowerCase())
+                                    || (eachEro.last_name.toLowerCase()).includes(eachKeyword.toLowerCase())
+                                    || (eachEro.email.toLowerCase()).includes(eachKeyword.toLowerCase())
+                                    
+                                ) {
+                                    isIncludesKeyword = true;
+                                }
+                            }
+                        })
+
+                        if (isIncludesKeyword) {
+                            eroList.push(eachEro);
+                        }
+                    });
+                } else {
+                    eroList = this.data.eroList;
+                }
+                return eroList;
+            }
+        }
     }
 </script>
