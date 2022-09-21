06e16fcb71e9c55dd688b3d8c93f395964607fc5
Phearun-chhun <phearun.chhun@student.passerellesnumeriques.org>
2022-09-21 10:09:59 +0700

    edit code

--- front/src/store/userStore.js
+++ front/src/store/userStore.js
@@ -26,11 +26,7 @@ export const userInformations = defineStore('get-data', {
   },
   actions: {
     getUserData(){
-<<<<<<< HEAD
-      axios.get('/users/'+21).then((res)=>{
-=======
       axios.get('/users/'+1).then((res)=>{
->>>>>>> dd296365597e9fd107f74b8feb4485d92bbecf47
         this.userStore = res.data;
       })
     },
