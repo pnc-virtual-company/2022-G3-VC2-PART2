a70e7a3c88d103fb10b93a7543969cc939723467
SEYHA.SOEURN <SEYHA.SOEURN@pnc.lan>
2022-09-22 09:25:56 +0700

    #59 : Review before merge alumniProfile

--- back/database/seeders/AlumniTableSeeder.php
+++ back/database/seeders/AlumniTableSeeder.php
@@ -16,8 +16,8 @@ class AlumniTableSeeder extends Seeder
     public function run()
     {
         $alumnis = [
-            ['id' => 1, 'user_id' => 1, 'major' => 'WEB', 'batch' => 2022, 'address' => 'Tuol Kork, Phnom Penh', 'telegram' => '097979797', 'phone' => '097979797', 'gender' => 'male', 'birth_date' => '2022-08-09'],
-            ['id' => 2, 'user_id' => 2, 'major' => 'SNA', 'batch' => 2023, 'address' => 'Sen Sok, Phnom Penh', 'telegram' => '088989796', 'phone' => '088989796', 'gender' => 'male', 'birth_date' => '2022-02-23'],
+            ['id' => 1, 'user_id' => 1, 'major' => 'web', 'batch' => 2022, 'address' => 'Tuol Kork, Phnom Penh', 'telegram' => '097979797', 'phone' => '097979797', 'gender' => 'male', 'birth_date' => '2022-08-09'],
+            ['id' => 2, 'user_id' => 2, 'major' => 'sna', 'batch' => 2023, 'address' => 'Sen Sok, Phnom Penh', 'telegram' => '088989796', 'phone' => '088989796', 'gender' => 'male', 'birth_date' => '2022-02-23'],
         ];
         foreach ($alumnis as $alumni) {
             Alumni::create($alumni);
